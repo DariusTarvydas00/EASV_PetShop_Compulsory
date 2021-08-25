378c5c2210f9f51c8b0733bf852c7675b443fade
DariusTarvydas00 <79032020+DariusTarvydas00@users.noreply.github.com>
2021-08-25 13:05:33 +0200

    Finished Day One

--- Src/Utils/StringUtil.cs
+++ Src/Utils/StringUtil.cs
@@ -30,7 +30,7 @@ namespace CrashCourse2021ExercisesDayOne.Utils
 
         internal string StringContains(string value1, string value2)
         {
-            return value1.Contains(value2) ? "YES" : "NO";
+            return value1.Contains(value2) ? "YES" : "NSO" ;
         }
 
         internal string StringToUpperCase(string value1)
